1b0ff7d3e4b53f247edc3ddff2ef999117c611a3
Sergey <sergey.cosilov@gmail.com>
2023-02-28 12:24:17 +0300

    test form

--- index.js
+++ index.js
@@ -1,27 +1,123 @@
 console.log('init')
 
+// document.querySelectorAll('a[href="#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjE4OTA3IiwidG9nZ2xlIjpmYWxzZX0%3D2"]').forEach((element) => {
+//     element.addEventListener('click', () => {
+//         let dialog = document.querySelector('.dialog-type-lightbox')
+//         if (dialog) {
+//             dialog.style.display = 'flex'
+//         }
+//         setTimeout(connectFormHardcor, 1000);
+//         console.log('Form connected to button');
+//     })
+// })
 
-document.querySelectorAll('a[href="#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjE4OTA3IiwidG9nZ2xlIjpmYWxzZX0%3D"]').forEach((element) => {
-    element.addEventListener('click', () => {
-        let dialog = document.querySelector('.dialog-type-lightbox')
-            if (dialog) {
-                dialog.style.display = 'flex'
-            }
-        setTimeout(connectForm, 1000);
-        console.log('Form connected to button');
-    })
+// function connectFormHardcor() {
+//     var callInit = false;
+//     var form = document.querySelector('.elementor-popup-modal form');
+//     if (form) {
+//         window.leadCM.formTelInputSelector = function(form) {
+//             var phone = form.querySelector('input[type="tel"]').value.replace(/ /g,'').replace('+', '');
+//             if (phone == null) phone = form.querySelector('input[type="Tel"]').value.replace(/ /g,'').replace('+', '');
+//
+//             if (phone.substring(0, 3) == '971') {
+//                 phone = '971' + phone.substring(3).replace(/^0+/, ''); // remove all zeros after country code
+//                 // console.log('phone number without zeros after country code: ', phone);
+//             }
+//
+//             if (phone.substring(0, 1) == '0') {
+//                 phone = phone.replace(/^0+/, ''); // remove all zeros in the beginning of phone number
+//                 // console.log('phone without zeros in the beginning: ', phone);
+//                 if (phone.substring(0, 3) == '971') {
+//                     phone = '971' + phone.substring(3).replace(/^0+/, ''); // remove all zeros after country code
+//                     // console.log('phone number without zeros after country code: ', phone);
+//                 } else if (phone.substring(0, 3) != '971') {
+//                     phone = '971' + phone; // add country code if no country code
+//                     // console.log('phone with country code: ', phone);
+//                 }
+//             }
+//             return phone;
+//
+//         };
+//
+//         form.addEventListener('submit', function (e) {
+//             // e.preventDefault()
+//             var phoneNumber = window.leadCM.formTelInputSelector(form);
+//             if (phoneNumber && window.leadCM && window.leadCM.call && !callInit) {
+//                 callInit = true;
+//                 if(window.leadCM.formBeforeCall) {
+//                     window.leadCM.formBeforeCall(phoneNumber, form, function() {window.leadCM.call(phoneNumber, 'universal_form');});
+//                    } else {
+//                     window.leadCM.call(phoneNumber, 'universal_form')
+//                       }
+//             }
+//         });
+//
+//         window.leadCM.formBeforeCall = function (phoneNumber, form, call) {
+//             var nameInput = form.querySelector('input[name="wpforms[fields][0]"]');
+//             var emailInput = form.querySelector('input[name="wpforms[fields][1]"]');;
+//             var interestedInput = form.querySelector('textarea[name="wpforms[fields][2]"]');
+//
+//
+//             var userName = nameInput ? nameInput.value : null;
+//             var userEmail = emailInput ? emailInput.value : null;
+//             var userInterested = interestedInput ? interestedInput.value : null;
+//
+//             var custom_params = {
+//                 lc_param_phone: phoneNumber,
+//             };
+//
+//             if (userName) {
+//                 custom_params['lc_param_name'] = userName;
+//             }
+//
+//             if (userEmail) {
+//                 custom_params['lc_param_email'] = userEmail;
+//             }
+//
+//             if (userInterested) {
+//                 custom_params['lc_param_interested'] = userInterested;
+//             }
+//
+//             leadCM.dispatchCustomEvent("CUSTOM_PARAMS", custom_params, call);
+//
+//         };
+//
+//     }
+//
+// }
+
+
+let form = document.querySelector('.elementor-popup-modal form');
+
+form.addEventListener('submit', function (e) {
+    console.log('user script')
+    setTimeout(() => {
+        window.location.href = 'https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event';
+    }, 1000)
 })
 
-document.querySelectorAll('a[href="#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjE4OTA3IiwidG9nZ2xlIjpmYWxzZX0%3D2"]').forEach((element) => {
+
+
+document.querySelectorAll('button[id="#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjE4OTA3IiwidG9nZ2xlIjpmYWxzZX0%3D"]').forEach((element) => {
     element.addEventListener('click', () => {
         let dialog = document.querySelector('.dialog-type-lightbox')
         if (dialog) {
             dialog.style.display = 'flex'
         }
-        setTimeout(connectFormHardcor, 1000);
-        console.log('Form connected to button');
+        setTimeout(connectForm, 1000);
+        console.log('Form connected to button new');
     })
 })
+
+
+
+
+
+
+
+
+
+
 
 function connectForm() {
 
@@ -30,10 +126,9 @@ function connectForm() {
 
     if (form) {
 
-        let formButton = form.querySelector('button[type="submit"]');
-
-        formButton.addEventListener('click', function (e) {
-
+        // let formButton = form.querySelector('button[type="submit"]');
+        form.addEventListener('submit', function (e) {
+            // console.log('submit convolo script')
             if ( window.leadCM && window.leadCM.call && !callInit) {
                 callInit = true;
                 var nameInput = form.querySelector('input[name="wpforms[fields][0]"]');
@@ -69,86 +164,3 @@ function connectForm() {
     }
 
 }
-
-function connectFormHardcor() {
-    console.log('connectFormHardcor')
-    var callInit = false;
-    var form = document.querySelector('.elementor-popup-modal form');
-
-    if (form) {
-
-        var formButton = form.querySelector('button[type="submit"]');
-
-        window.leadCM.formTelInputSelector = function(form) {
-
-            var phone = form.querySelector('input[type="tel"]').value.replace(/ /g,'').replace('+', '');
-            if (phone == null) phone = form.querySelector('input[type="Tel"]').value.replace(/ /g,'').replace('+', '');
-
-            if (phone.substring(0, 3) == '971') {
-                phone = '971' + phone.substring(3).replace(/^0+/, ''); // remove all zeros after country code
-                // console.log('phone number without zeros after country code: ', phone);
-            }
-
-            if (phone.substring(0, 1) == '0') {
-                phone = phone.replace(/^0+/, ''); // remove all zeros in the beginning of phone number
-                // console.log('phone without zeros in the beginning: ', phone);
-                if (phone.substring(0, 3) == '971') {
-                    phone = '971' + phone.substring(3).replace(/^0+/, ''); // remove all zeros after country code
-                    // console.log('phone number without zeros after country code: ', phone);
-                } else if (phone.substring(0, 3) != '971') {
-                    phone = '971' + phone; // add country code if no country code
-                    // console.log('phone with country code: ', phone);
-                }
-            }
-            console.log('phone: ', phone);
-            return phone;
-
-        };
-
-        formButton.addEventListener('click', function (e) {
-            var phoneNumber = window.leadCM.formTelInputSelector(form);
-            if (phoneNumber && window.leadCM && window.leadCM.call && !callInit) {
-                callInit = true;
-                if(window.leadCM.formBeforeCall) {
-                    window.leadCM.formBeforeCall(phoneNumber, form, function() {window.leadCM.call(phoneNumber, 'universal_form');});
-                } else {
-                    window.leadCM.call(phoneNumber, 'universal_form')
-                }
-            }
-        });
-
-        window.leadCM.formBeforeCall = function (phoneNumber, form, call) {
-
-            var nameInput = form.querySelector('input[name="wpforms[fields][0]"]');
-            var emailInput = form.querySelector('input[name="wpforms[fields][1]"]');;
-            var interestedInput = form.querySelector('textarea[name="wpforms[fields][2]"]');
-
-
-            var userName = nameInput ? nameInput.value : null;
-            var userEmail = emailInput ? emailInput.value : null;
-            var userInterested = interestedInput ? interestedInput.value : null;
-
-            var custom_params = {
-                lc_param_phone: phoneNumber,
-            };
-
-            if (userName) {
-                custom_params['lc_param_name'] = userName;
-            }
-
-            if (userEmail) {
-                custom_params['lc_param_email'] = userEmail;
-            }
-
-            if (userInterested) {
-                custom_params['lc_param_interested'] = userInterested;
-            }
-
-            leadCM.dispatchCustomEvent("CUSTOM_PARAMS", custom_params, call);
-
-        };
-
-    }
-
-}
-
